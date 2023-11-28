e20d574fbec60c235704552f027c133d842ef72b
Aloento <11802769+Aloento@users.noreply.github.com>
2023-11-28 15:28:36 +0100

    Refactor navigation links in AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,11 +1,11 @@
 <script setup>
-import {ref} from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import {Link} from '@inertiajs/vue3';
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Link } from "@inertiajs/vue3";
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -29,8 +29,18 @@ const showingNavigationDropdown = ref(false);
 
               <!-- Navigation Links -->
               <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                <NavLink :active="route().current('dashboard')" :href="route('dashboard')">
+                <NavLink
+                  :active="route().current('dashboard')"
+                  :href="route('dashboard')"
+                >
                   Dashboard
+                </NavLink>
+
+                <NavLink
+                  :active="route().current('chirps.index')"
+                  :href="route('chirps.index')"
+                >
+                  Chirps
                 </NavLink>
               </div>
             </div>
@@ -40,32 +50,38 @@ const showingNavigationDropdown = ref(false);
               <div class="ms-3 relative">
                 <Dropdown align="right" width="48">
                   <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button
-                                              class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                              type="button"
-                                            >
-                                                {{ $page.props.auth.user.name }}
+                    <span class="inline-flex rounded-md">
+                      <button
+                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                        type="button"
+                      >
+                        {{ $page.props.auth.user.name }}
 
-                                                <svg
-                                                  class="ms-2 -me-0.5 h-4 w-4"
-                                                  fill="currentColor"
-                                                  viewBox="0 0 20 20"
-                                                  xmlns="http://www.w3.org/2000/svg"
-                                                >
-                                                    <path
-                                                      clip-rule="evenodd"
-                                                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                      fill-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
+                        <svg
+                          class="ms-2 -me-0.5 h-4 w-4"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            clip-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            fill-rule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                    </span>
                   </template>
 
                   <template #content>
-                    <DropdownLink :href="route('profile.edit')"> Profile</DropdownLink>
-                    <DropdownLink :href="route('logout')" as="button" method="post">
+                    <DropdownLink :href="route('profile.edit')">
+                      Profile</DropdownLink
+                    >
+                    <DropdownLink
+                      :href="route('logout')"
+                      as="button"
+                      method="post"
+                    >
                       Log Out
                     </DropdownLink>
                   </template>
@@ -79,12 +95,17 @@ const showingNavigationDropdown = ref(false);
                 class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                 @click="showingNavigationDropdown = !showingNavigationDropdown"
               >
-                <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <svg
+                  class="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
                   <path
                     :class="{
-                                            hidden: showingNavigationDropdown,
-                                            'inline-flex': !showingNavigationDropdown,
-                                        }"
+                      hidden: showingNavigationDropdown,
+                      'inline-flex': !showingNavigationDropdown,
+                    }"
                     d="M4 6h16M4 12h16M4 18h16"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -92,9 +113,9 @@ const showingNavigationDropdown = ref(false);
                   />
                   <path
                     :class="{
-                                            hidden: !showingNavigationDropdown,
-                                            'inline-flex': showingNavigationDropdown,
-                                        }"
+                      hidden: !showingNavigationDropdown,
+                      'inline-flex': showingNavigationDropdown,
+                    }"
                     d="M6 18L18 6M6 6l12 12"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -108,12 +129,25 @@ const showingNavigationDropdown = ref(false);
 
         <!-- Responsive Navigation Menu -->
         <div
-          :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
+          :class="{
+            block: showingNavigationDropdown,
+            hidden: !showingNavigationDropdown,
+          }"
           class="sm:hidden"
         >
           <div class="pt-2 pb-3 space-y-1">
-            <ResponsiveNavLink :active="route().current('dashboard')" :href="route('dashboard')">
+            <ResponsiveNavLink
+              :active="route().current('dashboard')"
+              :href="route('dashboard')"
+            >
               Dashboard
+            </ResponsiveNavLink>
+
+            <ResponsiveNavLink
+              :href="route('chirps.index')"
+              :active="route().current('chirps.index')"
+            >
+              Chirps
             </ResponsiveNavLink>
           </div>
 
@@ -123,12 +157,20 @@ const showingNavigationDropdown = ref(false);
               <div class="font-medium text-base text-gray-800">
                 {{ $page.props.auth.user.name }}
               </div>
-              <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
+              <div class="font-medium text-sm text-gray-500">
+                {{ $page.props.auth.user.email }}
+              </div>
             </div>
 
             <div class="mt-3 space-y-1">
-              <ResponsiveNavLink :href="route('profile.edit')"> Profile</ResponsiveNavLink>
-              <ResponsiveNavLink :href="route('logout')" as="button" method="post">
+              <ResponsiveNavLink :href="route('profile.edit')">
+                Profile</ResponsiveNavLink
+              >
+              <ResponsiveNavLink
+                :href="route('logout')"
+                as="button"
+                method="post"
+              >
                 Log Out
               </ResponsiveNavLink>
             </div>
@@ -139,13 +181,13 @@ const showingNavigationDropdown = ref(false);
       <!-- Page Heading -->
       <header v-if="$slots.header" class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <slot name="header"/>
+          <slot name="header" />
         </div>
       </header>
 
       <!-- Page Content -->
       <main>
-        <slot/>
+        <slot />
       </main>
     </div>
   </div>
