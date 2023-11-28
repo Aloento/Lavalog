7ab6d87d6652cd2fb3c072c51acfac1573a9bdfc
Aloento <11802769+Aloento@users.noreply.github.com>
2023-11-28 18:09:27 +0100

    Refactor ChirpController methods

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -10,67 +10,75 @@ use Inertia\Response;
 
 class ChirpController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): Response
-    {
-      return Inertia::render('Chirps/Index', [
-        'chirps' => Chirp::with('user:id,name')->latest()->get(),
-      ]);
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(): Response
+  {
+    return Inertia::render('Chirps/Index', [
+      'chirps' => Chirp::with('user:id,name')->latest()->get(),
+    ]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request): RedirectResponse
+  {
+    $validated = $request->validate([
+      'message' => 'required|string|max:255',
+    ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): RedirectResponse
-    {
-        $validated = $request->validate([
-          'message' => 'required|string|max:255',
-        ]);
+    $request->user()->chirps()->create($validated);
 
-        $request->user()->chirps()->create($validated);
+    return redirect(route('chirps.index'));
+  }
 
-        return redirect(route('chirps.index'));
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Chirp $chirp)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Chirp $chirp)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Chirp $chirp)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Chirp $chirp)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Chirp $chirp)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Chirp $chirp): RedirectResponse
+  {
+    $this->authorize('update', $chirp);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Chirp $chirp)
-    {
-        //
-    }
+    $validated = $request->validate([
+      'message' => 'required|string|max:255',
+    ]);
+
+    $chirp->update($validated);
+
+    return redirect(route('chirps.index'));
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Chirp $chirp)
+  {
+    //
+  }
 }
